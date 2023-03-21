14519ac3a9915d051bc449c74bc473d5a42ba727
Ujjayanta <aprajit09041996@gmail.com>
2023-03-21 17:18:30 +0100

    Update PosterGenerator.py

--- pages/PosterGenerator.py
+++ pages/PosterGenerator.py
@@ -5,7 +5,7 @@ import random
 st.title("Generate Wartime Posters")
 st.header('Generated Posters')    
 st.write('An AI model has been created to generate World War posters. The training dataset was provided by KU Leuven. Press the button to generate a poster.')
-imageList = ['1.png','2.png','3.png']
+imageList = ['1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png','9.png','10.png']
 
 image = Image.open(random.choice(imageList))
 if st.button('Generate'):
