e03d1dd0e605c3676a1bcc6b0c163bcb5a8d7654
Ujjayanta <aprajit09041996@gmail.com>
2023-03-22 20:17:31 +0100

    Update WordClouds.py

--- pages/WordClouds.py
+++ pages/WordClouds.py
@@ -1,10 +1,30 @@
 import streamlit as st
 from PIL import Image
 import random
+import base64
   
-st.title("Generate Wartime Posters")
-st.header('Generated Posters')    
-st.write('An AI model has been created to generate World War posters. The training dataset was provided by KU Leuven. Press the button to generate a poster.')
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('bg.png') #
+  
+  
+  
+st.title("Topic Modelling")
+st.header('Wordclouds')    
+st.write('There are two types of documents: news publications vs regulations​. Some observations:')
+st.write('Always Dutch, French and German for news (mass media communication)​. For news publications: war vocabulary, time ("gisteren"), place, news vocabulary. For regulations​: juridical lexicon, syntaxis related to orders, daily life lexicon ("boter"), war vocabulary​')
 imageList = ['1.png','2.png','3.png']
 
 image = Image.open(random.choice(imageList))
