981757d1dcbf587e8304e27516930d44e29f1df3
Ujjayanta <aprajit09041996@gmail.com>
2023-03-22 20:16:25 +0100

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -2,6 +2,24 @@ import streamlit as st
 from PIL import Image
 import random
 
+import base64
+  
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('bg.png') #  
+
 st.header('ChaoTech Warriors')
 
 st.write(
